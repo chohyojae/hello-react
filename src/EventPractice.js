abc385f72b0f47c0693167618fd4852382db1d53
hjcho <hjcho@seegene.com>
2021-06-16 21:01:03 +0900

    state handler with arrow funct.

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -5,27 +5,57 @@ class EventPractice extends Component {
     message: '',
   };
 
+  handleChange = (e) => {
+    this.setState({ message: e.target.value });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({ message: '' }, () => {
+      alert('삭제 완료' + this.state.message);
+    });
+  };
+
+  // constructor(props) {
+  //   super(props);
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
+
+  // handleChange(e) {
+  //   this.setState({ message: e.target.value });
+  // }
+
+  // handleClick() {
+  //   alert(this.state.message);
+  //   this.setState({ message: '' }, () => {
+  //     alert('삭제 완료' + this.state.message);
+  //   });
+  // }
+
   render() {
     return (
       <div>
-        <h1> 이벤트 연습</h1>
+        <h1>&nbsp;이벤트 연습</h1>
         <input
           text="text"
           message="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({ message: e.target.value });
-            console.log(e.target.value);
-          }}
+          onChange={this.handleChange}
+          // onChange={(e) => {
+          //   this.setState({ message: e.target.value });
+          //   console.log(e.target.value);
+          // }}
         />
         <button
-          onClick={(e) => {
-            alert(this.state.message);
-            this.setState({ message: '' }, () => {
-              alert('삭제 완료' + this.state.message);
-            });
-          }}
+          onClick={this.handleClick}
+          // onClick={(e) => {
+          //   alert(this.state.message);
+          //   this.setState({ message: '' }, () => {
+          //     alert('삭제 완료' + this.state.message);
+          //   });
+          // }}
         >
           확인
         </button>
