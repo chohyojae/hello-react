ef9135da8ebde0db79325bbad647127453c08cc8
hjcho <hjcho@seegene.com>
2021-06-16 20:37:59 +0900

    state changes & effect between 2 comps.

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class EventPractice extends Component {
+  state = {
+    message: '',
+  };
+
   render() {
     return (
       <div>
@@ -9,10 +13,22 @@ class EventPractice extends Component {
           text="text"
           message="message"
           placeholder="아무거나 입력해 보세요"
+          value={this.state.message}
           onChange={(e) => {
+            this.setState({ message: e.target.value });
             console.log(e.target.value);
           }}
         />
+        <button
+          onClick={(e) => {
+            alert(this.state.message);
+            this.setState({ message: '' }, () => {
+              alert('삭제 완료' + this.state.message);
+            });
+          }}
+        >
+          확인
+        </button>
       </div>
     );
   }
