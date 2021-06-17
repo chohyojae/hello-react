a0ecf06199320cf018a5f52651019853f651f1f8
hjcho <hjcho@seegene.com>
2021-06-17 15:24:01 +0900

    component iteration

--- src/App.js
+++ src/App.js
@@ -2,18 +2,10 @@
 // import EventPractice from './EventPractice.js';
 // import ValidationSample from './ValidationSample';
 import React, { Component } from 'react';
-import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample';
 class App extends Component {
   render() {
-    return (
-      <div>
-        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
-
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          아래로 스크롤
-        </button>
-      </div>
-    );
+    return <IterationSample />;
   }
 }
 
