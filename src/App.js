efe23e5d619e7f43f0461cdddfcfda2c4cd65395
hjcho <hjcho@seegene.com>
2021-06-17 14:45:59 +0900

    call methods in a component via ref

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 // import Say from './Say.js';
 // import EventPractice from './EventPractice.js';
-import ValidationSample from './ValidationSample';
+// import ValidationSample from './ValidationSample';
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox ref={(ref) => (this.scrollBox = ref)} />
 
-function App() {
-  return <ValidationSample />;
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          아래로 스크롤
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
